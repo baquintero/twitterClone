034826c163dd02d25d3135ef9f6afb56766b3a2a
Brayan Quintero <baquintero.20@gmail.com>
2021-07-12 10:45:53 -0400

    Implementa el caso de uso Registro de usuario - Crea función en el controlador usuario que permite el registro del usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,75 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller{
-    
+
+    public function register(Request $request){
+        
+        /* $json = $request->input('json',null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true); */
+        $params_array = $request->all();
+
+        if(!empty($params_array)){
+            //Limpiar los datos
+            $params_array = array_map('trim', $params_array);
+             
+            //Validar los datos
+            $validate = \Validator::make($params_array, [
+                'name'      => 'required',
+                'surname'   => 'required',
+                'name_user' => 'required|unique:users',
+                'email'     => 'required|email|unique:users',
+                'password'  => 'required'
+            ]);
+
+            if($validate->fails()){
+                //La validacion ha fallado
+                $signin = array(
+                    'status'    => 'error',
+                    'code'      =>  404,
+                    'message'   =>  'El usuario no se ha podido registrar',
+                    'errors'    => $validate->errors()
+                );
+
+            }else{
+                //Cifrar la password
+                $pwd = hash('sha256', $params_array['password']);
+
+                //Crear el usuario 
+                $user = new User();
+                $user->name             = $params_array['name'];
+                $user->surname          = $params_array['surname'];
+                $user->name_user        = $params_array['name_user'];
+                $user->email            = $params_array['email'];
+                $user->created          = date("Y-m-d H:i:s");
+                $user->update_session   = date("Y-m-d H:i:s"); 
+                $user->password         = $pwd;
+
+                //Guardar el usuario
+                $user->save();
+
+                //Validacion correcta
+                $signin = array(
+                    'status'    => 'success',
+                    'code'      =>  202,
+                    'message'   =>  'El usuario se registro correctamente'
+                );
+
+
+            }
+        }else{
+            $signin = array(
+                'status'    => 'error',
+                'code'      =>  404,
+                'message'   =>  'Los datos enviados no son correctos',
+                'errors'    => $validate->errors()
+            );
+        }
+       return $signin;
+    }
 
     public function login(Request $request){
         
